f7913f8e6e534728f6dee532532ba5666a56a4e1
DaniaErnest <daniaernest@aol.com>
2022-05-03 21:06:25 +0100

    About page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root 'pages#hello' #setting our root routr to application controller
+  root 'pages#home' #setting our root routr to application controller
+  get 'about', to: 'pages#about' #set the about to the controller name(page) and the method
 end
